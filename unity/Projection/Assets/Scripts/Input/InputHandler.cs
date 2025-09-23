95f5eab00e63547a20438991bf996ab5862fd488
mikegent01 <70764301+mikegent01@users.noreply.github.com>
2025-09-23 18:15:23 -0400

    First Commit of many
    * Started Work On Card Get Screen
    This game will be massively reworked and there will be no more engine changes!!!!!
    I am  almost at the implementation stage I expect this to take a few months

---
+++ unity/Projection/Assets/Scripts/Input/InputHandler.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputHandler : MonoBehaviour
+{
+    #region Variables
+
+    private Camera _mainCamera;
+
+    // animation_cards this will be found in 
+    public bool animation_card_liftoff = false;
+    public animation_card animCard;
+    #endregion
+
+    private void Awake()
+    {
+        _mainCamera = Camera.main;
+    }
+
+    public void OnClick(InputAction.CallbackContext context)
+    {
+        if (!context.started) return;
+
+        var rayHit = Physics2D.GetRayIntersection(_mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue()));
+        if (!rayHit.collider) return;
+
+        if (rayHit.collider.CompareTag("cardclickable"))
+        {
+            if (animCard.introended == true)
+            {
+                animation_card_liftoff = true;
+                animCard.playliftoff();
+                Debug.Log("Playing Liftoff");
+            }
+            else
+            {
+                Debug.Log("Card Is Still Moving");
+            }
+        }
+        else
+        {
+            Debug.Log(rayHit.collider.gameObject.name);
+        }
+    }
+}
---
+++ unity/Projection/Assets/Scripts/card/animation_card.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class animation_card : MonoBehaviour
+{
+    [SerializeField] private Animator cardani;
+    [SerializeField] private string cardliftoff = "Card_Outtro";
+    public bool introended = false;
+    public InputHandler inputhan;
+    public void introend()
+    {
+        introended = true;
+        Debug.Log("Animation Finished You May Click");
+    }
+    public void playliftoff()
+    {
+        if (inputhan.animation_card_liftoff == true)
+        {
+            inputhan.animation_card_liftoff = false;
+            cardani.Play(cardliftoff, 0, 0.0f);
+        }     
+    }
+
+}
